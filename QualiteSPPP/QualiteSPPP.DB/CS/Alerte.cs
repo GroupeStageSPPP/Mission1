f91503ece06185243dc36fa2372a940593ca93fa
JB <jb.blois@iia-laval.fr>
2014-05-26 17:32:19 +0200

    Ajout Alerte et Test_Echantillon

---
+++ QualiteSPPP/QualiteSPPP.DB/CS/Alerte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualiteSPPP.DB
+{
+    public class Alerte
+    {
+        public Int32 Identifiant { get; set; }
+        public DateTime DateAlerte { get; set; }
+        public String Message { get; set; }
+        public Char Type { get; set; }
+        public Test_Echantillon test_echantillon { get; set; }
+    }
+}
