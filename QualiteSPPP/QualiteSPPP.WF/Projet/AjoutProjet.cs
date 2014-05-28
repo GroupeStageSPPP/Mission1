514d9cd84b41365614667780c0c18d51e41edcd9
JB <jb.blois@iia-laval.fr>
2014-05-28 10:56:00 +0200

    Ajout WF Projet

---
+++ QualiteSPPP/QualiteSPPP.WF/Projet/AjoutProjet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QualiteSPPP.WinForm
+{
+    public partial class AjoutProjet : Form
+    {
+        public AjoutProjet()
+        {
+            InitializeComponent();
+        }
+
+        private void AjoutProjet_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PiecePeintureProjet PPP = new PiecePeintureProjet();
+            this.Hide();
+            PPP.ShowDialog();
+        }
+    }
+}
